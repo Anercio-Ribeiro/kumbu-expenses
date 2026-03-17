11a588c125e5c8df55b5163184691d47bed01cbb
Anércio Ribeiro <anercio.ribeiro94@outlook.com>
2026-03-18 00:13:24 +0100

    fix, skeletons

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
+// import type { NextConfig } from 'next'
+
+// const nextConfig: NextConfig = {
+//   experimental: { typedRoutes: true },
+//   images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
+// }
+
+// export default nextConfig
+
+
+
+
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: { typedRoutes: true },
+  experimental: {},
   images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
 }
 
